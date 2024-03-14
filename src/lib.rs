465b2c2b9455633ed1ca83d22c5fee38741d8887
Peter Fraser-Mackenzie <pete.fm@pm.me>
2024-03-14 16:01:30 +0000

    v0.1.1 - Remove leading whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,33 @@ fn line_starts_with_non_whitespace(line: &str) -> bool {
     line.chars().next().map_or(false, |c| !c.is_whitespace())
 }
 
+fn adjust_leading_whitespace(input: &str) -> String {
+    let leading_whitespace_count = input
+        .lines()
+        .find_map(|line| {
+            if !line.trim().is_empty() {
+                Some(line.chars().take_while(|c| c.is_whitespace()).count())
+            } else {
+                None
+            }
+        })
+        .unwrap_or(0);
+
+    input
+        .lines()
+        .map(|line| {
+            if line.len() >= leading_whitespace_count {
+                &line[leading_whitespace_count..]
+            } else {
+                line
+            }
+        })
+        .collect::<Vec<&str>>()
+        .join("\n")
+}
+
 pub fn process_string(input: &str) -> String {
+    let input = adjust_leading_whitespace(input);
     let mut result = Vec::new();
     let mut previous_line: Option<String> = None;
     for line in input.lines() {
